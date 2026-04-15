d84ab9f4c58aa93782ff3c8dc43088f2e12fa997
Ali Warish <syedaliwarish3@gmail.com>
2026-04-15 10:21:19 +0530

    Refactor to use dynamic array and linear search

--- 24529_Assign.cpp
+++ 24529_Assign.cpp
@@ -1,48 +1,39 @@
-#include <iostream>
-using namespace std;
-
-
-int binarySearch(int arr[], int key, int low, int high) {
-    while (low <= high) {
-        int mid = low + (high - low) / 2;
-
-        if (arr[mid] == key)
-            return mid + 1; 
-        else if (arr[mid] < key)
-            low = mid + 1;
-        else
-            high = mid - 1;
-    }
-    return low; 
-}
-
-void binaryInsertionSort(int arr[], int n) {
-    for (int i = 1; i < n; i++) {
-        int key = arr[i];
-        int j = i - 1;
-
-        
-        int pos = binarySearch(arr, key, 0, j);
-
-       
-        while (j >= pos) {
-            arr[j + 1] = arr[j];
-            j--;
-        }
-
-        arr[pos] = key;
-    }
-}
+#include <stdio.h>
+#include <stdlib.h>
 
 int main() {
-    int arr[] = {12, 11, 13, 5, 6};
-    int n = sizeof(arr) / sizeof(arr[0]);
+    int n, key, i, flag = 0;
+    int *arr;
 
-    binaryInsertionSort(arr, n);
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
 
-    cout << "Sorted array: ";
-    for (int i = 0; i < n; i++)
-        cout << arr[i] << " ";
+    arr = (int *)malloc(n * sizeof(int));
+    if (arr == NULL) {
+        printf("Memory allocation failed!\n");
+        return 1;
+    }
+
+    printf("Enter %d elements:\n", n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter element to search: ");
+    scanf("%d", &key);
+
+    for (i = 0; i < n; i++) {
+        if (arr[i] == key) {
+            printf("Element %d found at position %d\n", key, i + 1);
+            flag = 1;
+            break;
+        }
+    }
+
+    if (!flag) {
+        printf("Element %d not found in array\n", key);
+    }
+
+    free(arr);
     return 0;
 }
-
